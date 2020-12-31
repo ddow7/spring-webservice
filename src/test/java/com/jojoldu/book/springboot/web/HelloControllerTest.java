f9d97b0ec151b90d2e8bbec529d4ab23a1df3cf4
bbbug17 <bbbug17@naver.com>
2020-12-31 22:05:05 +0900

    dto 테스트 코드 추가 작성

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -5,13 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // JUnit에 내장된 실행자 외에 다른 실행자를 실행, 스프링 부트 테스트와 JUnit 사이에 연결자 역할
-@WebMvcTest(controllers = HelloController.class) // 스프링 테스트 어노테이션, Web(MVC)에 집중하게 해줌
+@WebMvcTest
 public class HelloControllerTest {
 
     @Autowired // 빈 주입
@@ -24,5 +26,16 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청
             .andExpect(status().isOk()) // 체이닝을 통해 mvc.perform 결과를 검증, 200,404 등의 상태 점검
             .andExpect(content().string(hello)); // mvc.perform 결과를 검증, 응답 본문 내용 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
