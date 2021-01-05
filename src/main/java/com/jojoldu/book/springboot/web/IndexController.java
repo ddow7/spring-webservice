38ef1132e2bdc8ddf33c93220323a5ca7f89ffef
bbbug17 <bbbug17@naver.com>
2021-01-05 21:12:18 +0900

    머스테치 화면 구성, 메인 및 게시글 등록 페이지 구현

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -3,11 +3,17 @@ package com.jojoldu.book.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// 페이지 관련 컨트롤러
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
