877804241e4f04cdf62632740df3f0b3fcd8b4f2
bbbug17 <bbbug17@naver.com>
2021-01-08 18:25:47 +0900

    게시글 삭제 구현

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,6 +7,9 @@ var main = {
         $('#btn-update').on('click',function (){
             _this.update();
         });
+        $('#btn-delete').on('click',function (){
+            _this.delete();
+        });
     },
     save : function (){
         var data = {
@@ -43,10 +46,25 @@ var main = {
             data: JSON.stringify(data)
         }).done(function (){
             alert('글이 수정되었습니다.');
-            window.location.href = '/'; // 등록 성공시 메인페이지로 이동
+            window.location.href = '/';
         }).fail(function (error){
             alert(JSON.stringify(error));
         });
-    }
+    },
+    delete : function (){
+        var id = $('#id').val();
+
+        $.ajax({
+            type:'DELETE',
+            url:'/api/v1/posts/'+id,
+            dataType:'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(function (){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    },
 };
 main.init();
